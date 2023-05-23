c509c3c73a3436d7404563f93881bf04207ffaed
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-23 16:57:25 -0400

    simplify

--- print-dir.sh
+++ print-dir.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-# Script to export directory structure excluding certain directories and 
-files
 
 # Define directories and files to exclude
 EXCLUDE_NAMES="node_modules .next .git .env mnemonic*"
 
 # Prepare the find command
-FIND_COMMAND="find . \( "
+FIND_COMMAND="find ."
 
 # Add each directory or file to the find command
-for NAME in $EXCLUDE_NAMES; do
-    FIND_COMMAND+="-name '$NAME' -o "
+for NAME in $EXCLUDE_NAMES
+do
+    FIND_COMMAND+=" -path ./$NAME -prune -o"
 done
 
-# Remove the last "-o" and add the rest of the command
-FIND_COMMAND=${FIND_COMMAND::-3}
-FIND_COMMAND+="\) -prune -o -print > directory.txt"
+FIND_COMMAND+=" -print > directory.txt"
 
 # Run the command
 eval $FIND_COMMAND
-
